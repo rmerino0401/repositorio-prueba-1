8c78ce90ace908c2d8ee3328a62e205a6dcf586e
rmerino0401 <rmerino0401@gmail.com>
2021-06-21 16:54:08 -0500

    19.06.21 recuperacion de clase

---
+++ assets/images/semana3/Dia4/intro-a-js/intro.js
@@ -0,0 +1,51 @@
+console.log("Hola mundo"); //  con punto (.) es una funcion que esta dentro de "console"
+
+console.warn("hey!! soy una advertencia");
+
+console.error("")
+
+class Mesa {
+    // es como un plano para hacer mesas
+
+    imprimir.nombre
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let otrosaludo = "hola, como estas?, por favor evita usar la palabra reservada var. Cuando quieras usar una variable, usa [let] en su lugar)
+// "let" se usa para valores que si cambian, antes se usaba [var] pero ya es muy antiguo y ahora se usa let
+console.log(otrosaludo)
+
+otrosaludo= "3"
+
+console.log(otrosaludo)
+
+const nombre = "Josue" 
+// "const" se usa para valores que no cambian, por ejm mi nombreno se puede cambiar el valor de una constante
+console.log(nombre);
+
+nombre= "faraon love shady 2"
+
+console.log(nombre)
+
+console.log("Hola")
+
+
+
+
+
+
+
+
+
+
