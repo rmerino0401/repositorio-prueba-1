48ad77245a171e91625f17b579248e631e773997
rmerino0401 <rmerino0401@gmail.com>
2021-06-22 00:05:01 -0500

    ejercicios javascript 21.06.21

--- semana4/Dia1/04-ejercicios-variables.js
+++ semana4/Dia1/04-ejercicios-variables.js
@@ -1,3 +1,32 @@
+//1. Porque pagar a un adivino cuando puedes tener el tuyo propio?
+
+//Guardar en variables, A. la cantidad de hijos, B. el nombre de su pareja  C. localización geográfica D. un trabajo
+
+//=> Estarás casado con ______ tendrás _____ Hijos y viviran en _______ y estaras trabajando de _________
+
+// let cantHijos = +prompt("Cuantos hijos deseas")
+// let pareja = prompt("Con quien te casarás")
+// let trabajo = prompt("De qué te gustaria trabajar")
+// let dondeViviras = prompt("Donde te gustaria vivir")
+
+// let resultado = `Estarás casado con ${pareja} tendrás ${cantHijos} Hijos y vivirán en ${dondeViviras} y estarás trabajando de ${trabajo}`
+
+// console.log(resultado)
+
+//2. Una empresa desea saber la edad de un trabajador pero solo tiene el año de nacimiento, se asume que el trabajador nació el 01 de enero
+
+//=>La edad del trabajador es:
+
+// let anioDeNacimiento = +prompt("Que año nació");
+// console.log(2021 - anioDeNacimiento);
+
+//3.Conversor de Celsius a Farenheit
+
+//=>Que ingrese X Celsius y me muestre la conversión
+//a Farenheit
+
+
+// respuesta 1
 let canthijos = +prompt("¿Cuantos hijos deseas?");
 
 let pareja = prompt("¿Con quien te casaras?");
@@ -10,4 +39,31 @@ let dondeviviras = prompt("¿donde te gustaria vivir?");
 let resultado = `Estarás casado con ${pareja}, tendrás ${canthijos} ,vivirán en ${dondeviviras} y estarás trabajando de ${trabajo}`;
 
 console.log(resultado);
+
+// respuesta 2
+
+let anioDeNacimiento = +prompt("Que año nació");
+console.log(2021 - anioDeNacimiento)
+
+// respuesta 3
+
+let celsius = +prompt("Cuanto es la temperatura?")
+
+let Farenheit = celsius*9/5 + 32;
+
+console.log(Farenheit)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
