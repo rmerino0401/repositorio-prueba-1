b1be14026dda38434b86a9266ae0632e28c046ab
rmerino0401 <rmerino0401@gmail.com>
2021-06-23 08:59:52 -0500

    ejercicios javascript 22.06.21

--- semana4/Dia2/03-ejercicios.js
+++ semana4/Dia2/03-ejercicios.js
@@ -10,3 +10,28 @@
 // }
 
 
+let nro1 = 45;
+let nro2 = 10;
+let nro3 = 90;
+
+// necesitamos saber cual de estos 3 es el número mayor
+
+
+
+let cantuvas = +prompt("Cuantos asistentes/comenzales tendrá el evento?")
+
+let precioFinal = 0;
+
+if(cantPlatos > 300){
+  precioFinal = cantPlatos * 75;
+  console.log(`El precio sera de: ${precioFinal}`)
+}else if(cantPlatos < 300 && cantPlatos >= 200){
+  precioFinal = cantPlatos * 85;
+  console.log(`El precio sera de: ${precioFinal}`)
+}else if(cantPlatos < 200 && cantPlatos >= 100){
+  precioFinal = cantPlatos * 88;
+  console.log(`El precio sera de: ${precioFinal}`)
+}else{
+  precioFinal = cantPlatos * 90;
+  console.log(`El precio sera de: ${precioFinal}`)
+}
