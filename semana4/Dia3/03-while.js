e29ab22a766c54cf54a5c460432ac5b814f350fc
rmerino0401 <rmerino0401@gmail.com>
2021-06-24 18:12:14 -0500

    23.06.21

--- semana4/Dia3/03-while.js
+++ semana4/Dia3/03-while.js
@@ -1,16 +1,17 @@
+//while -> pregunta y si es verdad, ejecutas
 // let contador = 1;
 
-// while(contador <= 10){
-//     console.log(`el contador es : ${contador}`)
-//     contador = contador + 1
+// while(contador < 10){
+//   console.log(`el contador es : ${contador}`)
+//   //Siempre que utilicemos una estructura repetitiva, vamos a necesitar un condicional/limitante
+//   contador = contador + 1
 // }
-// // pero lo que este aca abajo despues del while no se va a ejecutar hasta que termine el while
-// console.log(`yo estoy despues del while ${contador}`)
 
+// //Pero lo que este acá abajo después del while, no se va a ejecutar hasta que termine el while
+// console.log(`Yo estoy después del while ${contador}`)
 
-
-// Do while
-let contador2 =1;
+//do while -> haz y luego preguntas
+let contador2 = 1;
 
 do{
     console.log(`el contador es : ${contador2}`);
