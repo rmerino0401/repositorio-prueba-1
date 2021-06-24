cdb3fdf788eddff28ad44f44c92b11f7c91a2c85
rmerino0401 <rmerino0401@gmail.com>
2021-06-24 18:57:54 -0500

    for js

---
+++ semana4/Dia4/01-for.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ for va a tener un contador, un condicional y una actualizacion del contador
+ el codigo va a repetirse varias veces
+*/
+ 
+// for(let cont =0; cont <= 10; cont++){
+//     console.log(`El contador es ${cont}`)
+// }
+
+let nombres = ["Daniel", "Lezly", "Jorge", "Eduardo", "Osmar", "Josue", "Linder"]
+
+for(let i = 0; i < nombres.length; i++){
+    console.log(nombres[i])
+}
+
+let carta = ["ceviche", "cuy", "lasagna", "adobo", "seco", "seco de chabelo", "carapulcra", "aji de gallina", "rocoto relleno"];
+
+let eleccion =+prompt(`indique la opcion deseada:
+                                    `)
+for(let)
