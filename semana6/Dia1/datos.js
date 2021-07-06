b49c37b42a59581669feab61fdf96847f18b9c0e
rmerino0401 <rmerino0401@gmail.com>
2021-07-05 20:59:07 -0500

    05.07.21

---
+++ semana6/Dia1/datos.js
@@ -0,0 +1,482 @@
+let peliculas = [
+  {
+    "title": "2067",
+    "vote_average": 4.8,
+    "popularity": 1545.946,
+    "vote_count": 356,
+    "release_date": "2020-10-01",
+    "adult": false,
+    "backdrop_path": "/5UkzNSOK561c2QRy2Zr4AkADzLT.jpg",
+    "id": 528085,
+    "genre_ids": [
+      878,
+      53,
+      18
+    ],
+    "overview": "El viaje de un hombre hacia el futuro para salvar un mundo moribundo.",
+    "original_language": "en",
+    "original_title": "2067",
+    "poster_path": "/7D430eqZj8y3oVkLFfsWXGRcpEG.jpg",
+    "video": false
+  },
+  {
+    "title": "Mercenarios de élite",
+    "vote_average": 5,
+    "popularity": 1316.819,
+    "overview": "El trabajo del multimillonario CEO de tecnología Donovan Chalmers es tan valioso que contrata mercenarios para protegerlo, y un grupo terrorista secuestra a su hija solo para conseguirlo.",
+    "release_date": "2020-10-23",
+    "adult": false,
+    "backdrop_path": "/86L8wqGMDbwURPni2t7FQ0nDjsH.jpg",
+    "id": 724989,
+    "genre_ids": [
+      28,
+      53
+    ],
+    "vote_count": 163,
+    "original_language": "en",
+    "original_title": "Hard Kill",
+    "poster_path": "/x7Dc6FTBYOfwMDEHSGW6EOitidk.jpg",
+    "video": false
+  },
+  {
+    "video": false,
+    "vote_average": 8,
+    "popularity": 1190.392,
+    "vote_count": 1505,
+    "release_date": "2020-08-14",
+    "adult": false,
+    "backdrop_path": "/wu1uilmhM4TdluKi2ytfz8gidHf.jpg",
+    "id": 400160,
+    "genre_ids": [
+      16,
+      14,
+      12,
+      35,
+      10751
+    ],
+    "overview": "Los amigos son lo más importante para Bob Esponja, por lo que no dudará en salir de la comodidad de su hogar en Fondo de Bikini, junto con Patricio, para adentrarse en un mundo desconocido, arriesgando sus vidas, para salvar a su amigo de la infancia, Gary, de las garras del rey Poseidón que le ha secuestrado en la Ciudad Perdida de la Atlantida. ¿Serán capaces de lograrlo?",
+    "original_language": "en",
+    "original_title": "The SpongeBob Movie: Sponge on the Run",
+    "poster_path": "/sp9fYcVbUHDFgGZpLvCSJsplfbd.jpg",
+    "title": "Bob Esponja: Un héroe al rescate"
+  },
+  {
+    "video": false,
+    "vote_average": 7.1,
+    "popularity": 1081.895,
+    "overview": "Un matrimonio a punto de separarse debe tratar de garantizar la seguridad de su hijo y ellos mismos cuando una catástrofe se avecina hacia el planeta. Un objeto del espacio está a punto de colisionar con la Tierra y esta familia es escogida para entrar en un búnker subterráneo. Sin embargo, tienen poco tiempo. Tan solo cuentan con 48 horas antes de que el mundo que conocen se destruya para siempre.",
+    "release_date": "2020-07-29",
+    "adult": false,
+    "backdrop_path": "/2Fk3AB8E9dYIBc2ywJkxk8BTyhc.jpg",
+    "id": 524047,
+    "genre_ids": [
+      28,
+      53
+    ],
+    "title": "Greenland: el último refugio",
+    "original_language": "en",
+    "original_title": "Greenland",
+    "poster_path": "/ltlKaXvIHMTOgxqVbZSGUqcExuN.jpg",
+    "vote_count": 612
+  },
+  {
+    "video": false,
+    "vote_average": 6.9,
+    "popularity": 1080.532,
+    "vote_count": 762,
+    "release_date": "2020-10-26",
+    "adult": false,
+    "backdrop_path": "/8rIoyM6zYXJNjzGseT3MRusMPWl.jpg",
+    "id": 531219,
+    "genre_ids": [
+      14,
+      10751,
+      12,
+      35,
+      27
+    ],
+    "overview": "Cuenta la conmovedora historia de un pequeño huérfano que, a finales de 1967, se va a vivir con su querida abuela a Demopolis, un pueblo de Alabama. El niño y su abuela tienen extraños encuentros con algunas brujas aparentemente glamurosas pero absolutamente diabólicas, así que ella decide quitarse de en medio y llevarse al chico a un hotel de lujo en la costa. Por desgracia, llegan exactamente al mismo tiempo que la Gran Bruja que ha reunido a sus compinches de todo el planeta -que van de incógnito- para llevar a cabo sus horribles planes. Adaptación del libro \"Las brujas\" de Roald Dahl.",
+    "original_language": "en",
+    "original_title": "Roald Dahl's The Witches",
+    "poster_path": "/s1y92244iBlKW04rEnaEK8i8tPY.jpg",
+    "title": "Las brujas"
+  },
+  {
+    "video": false,
+    "vote_average": 5.8,
+    "popularity": 1050.176,
+    "vote_count": 231,
+    "release_date": "2020-10-30",
+    "adult": false,
+    "backdrop_path": "/gnf4Cb2rms69QbCnGFJyqwBWsxv.jpg",
+    "id": 671039,
+    "genre_ids": [
+      53,
+      28,
+      18,
+      80
+    ],
+    "overview": "Un policía leal, en el punto de mira de agentes corruptos y de belicosas bandas de Marsella, debe proteger a su brigada. Y se ocupará personalmente de la situación.",
+    "original_language": "fr",
+    "original_title": "Bronx",
+    "poster_path": "/hSpm2mMbkd0hLTRWBz0zolnLAyK.jpg",
+    "title": "Bronx"
+  },
+  {
+    "title": "Mulán",
+    "vote_average": 7.2,
+    "popularity": 792.693,
+    "vote_count": 2973,
+    "release_date": "2020-09-04",
+    "adult": false,
+    "backdrop_path": "/qAKvUu2FSaDlnqznY4VOp5PmjIF.jpg",
+    "id": 337401,
+    "genre_ids": [
+      28,
+      12,
+      18,
+      14
+    ],
+    "overview": "El emperador de China emite un decreto para reclutar a un varón por cada familia que deberá servir en el ejército imperial para defender al país de los invasores del norte. Hua Mulán, hija única de un condecorado guerrero, se presenta para evitar que su anciano padre sea llamado a filas. Se hace pasar por un hombre, Hua Jun y se somete a todo tipo de pruebas. Para conseguirlo deberá apelar a su fuerza interior y sacar a la luz todo su potencial. Se trata de una aventura épica que la transformará en una guerrera laureada que la hará merecedora del respeto de todo un país y motivo de orgullo para su padre.",
+    "original_language": "en",
+    "original_title": "Mulan",
+    "poster_path": "/rd7ElSRYhN2CFqMcyH8rqrwLbd6.jpg",
+    "video": false
+  },
+  {
+    "video": false,
+    "vote_average": 6.4,
+    "popularity": 772.845,
+    "vote_count": 161,
+    "release_date": "2020-09-29",
+    "adult": false,
+    "backdrop_path": "/aO5ILS7qnqtFIprbJ40zla0jhpu.jpg",
+    "id": 741067,
+    "genre_ids": [
+      28,
+      53,
+      12,
+      18
+    ],
+    "overview": "Jesse Freeman es un ex oficial de las fuerzas especiales y experto en explosivos que ahora tiene un trabajo regular como guardia de seguridad en un estadio de baloncesto de última generación. Los problemas surgen cuando un grupo de terroristas expertos en tecnología secuestra al propietario del equipo y a la hija de Jesse durante la noche de apertura. Enfrentando un reloj en marcha y probabilidades imposibles, depende de Jesse no solo salvarlos, sino también a una casa llena de fanáticos en este thriller de acción altamente cargado.",
+    "original_language": "en",
+    "original_title": "Welcome to Sudden Death",
+    "poster_path": "/elZ6JCzSEvFOq4gNjNeZsnRFsvj.jpg",
+    "title": "Welcome to Sudden Death"
+  },
+  {
+    "video": false,
+    "vote_average": 6.9,
+    "popularity": 756.847,
+    "vote_count": 166,
+    "release_date": "2020-10-23",
+    "adult": false,
+    "backdrop_path": "/DA7gzvlBoxMNL0XmGgTZOyv67P.jpg",
+    "id": 741074,
+    "genre_ids": [
+      16,
+      10751,
+      35,
+      14
+    ],
+    "overview": "Los primeros días de vida del pequeño Olaf, cuándo y por qué cobra vida y cómo empieza a buscar su propia identidad, después de la huída de Elsa del palacio de Arendelle.",
+    "original_language": "en",
+    "original_title": "Once Upon a Snowman",
+    "poster_path": "/yckfWcCTKEplBCilRdE4d43xowl.jpg",
+    "title": "Érase una vez un muñeco de nieve"
+  },
+  {
+    "title": "Come Play",
+    "vote_average": 6.8,
+    "popularity": 710.91,
+    "vote_count": 67,
+    "release_date": "2020-10-28",
+    "adult": false,
+    "backdrop_path": "/gkvOmVXdukAwpG8LjTaHo2l2cWU.jpg",
+    "id": 571384,
+    "genre_ids": [
+      27
+    ],
+    "overview": "Un niño problemático y su familia se convierten en el objetivo de un monstruo que se materializa a través de objetos electrónicos, tales como teléfonos móviles y tablets. Remake del cortometraje \"Larry\", creado por Jacob Chase en 2017.",
+    "original_language": "en",
+    "original_title": "Come Play",
+    "poster_path": "/e98dJUitAoKLwmzjQ0Yxp1VQrnU.jpg",
+    "video": false
+  },
+  {
+    "video": false,
+    "vote_average": 7.3,
+    "popularity": 710.638,
+    "vote_count": 2028,
+    "release_date": "2020-09-02",
+    "adult": false,
+    "backdrop_path": "/6hgItrYQEG33y0I7yP2SRl2ei4w.jpg",
+    "id": 613504,
+    "genre_ids": [
+      10749,
+      18
+    ],
+    "overview": "Ha pasado el tiempo, y Hardin todavía no se sabe si es realmente el chico profundo y reflexivo del que Tessa se enamoró, o ha sido un extraño todo este tiempo. Ella quiere alejarse, pero no es tan fácil. Tessa se ha centrado en sus estudios y comienza a trabajar como becaria en Vance Publishing. Allí conoce a Trevor, un nuevo y atractivo compañero de trabajo que es exactamente el tipo de persona con la que debería estar. Pero Hardin sabe que cometió un error, posiblemente el más grande de su vida y quiere corregir sus errores y vencer a sus demonios.",
+    "original_language": "en",
+    "original_title": "After We Collided",
+    "poster_path": "/5pg2rn4SM47B6Orc1xWXZ3MAVUc.jpg",
+    "title": "After: En mil pedazos"
+  },
+  {
+    "video": false,
+    "vote_average": 7.4,
+    "popularity": 707.156,
+    "vote_count": 109,
+    "release_date": "2020-11-06",
+    "adult": false,
+    "backdrop_path": "/6C7ZGYeR8QpT3X6C2RFEu6yiSKK.jpg",
+    "id": 622855,
+    "genre_ids": [
+      10751,
+      14,
+      10402
+    ],
+    "overview": "Un mundo imaginario cobra vida en la historia de un excéntrico creador de juguetes, su aventurera nieta y una invención mágica que tiene el poder de cambiar sus vidas para siempre.",
+    "original_language": "en",
+    "original_title": "Jingle Jangle: A Christmas Journey",
+    "poster_path": "/5RbyHIVydD3Krmec1LlUV7rRjet.jpg",
+    "title": "La Navidad mágica de los Jangle"
+  },
+  {
+    "video": false,
+    "vote_average": 5.8,
+    "popularity": 682.811,
+    "vote_count": 193,
+    "release_date": "2020-09-29",
+    "adult": false,
+    "backdrop_path": "/pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+    "id": 694919,
+    "genre_ids": [
+      28
+    ],
+    "title": "El avión del dinero",
+    "original_language": "en",
+    "original_title": "Money Plane",
+    "poster_path": "/v0BiswYPEZzZTYazvwktPTOTMpP.jpg",
+    "overview": "Un ladrón profesional con una deuda de 40 millones de dólares a sus espaldas y la vida de su familia en peligro, se ve obligado a realizar un último golpe: robar un futurista casino aéreo cuya que tiene por clientes a los criminales más peligrosos del mundo."
+  },
+  {
+    "video": false,
+    "vote_average": 6.3,
+    "popularity": 666.581,
+    "vote_count": 800,
+    "release_date": "2020-08-26",
+    "adult": false,
+    "backdrop_path": "/eCIvqa3QVCx6H09bdeOS8Al2Sqy.jpg",
+    "id": 340102,
+    "genre_ids": [
+      28,
+      878,
+      27,
+      12
+    ],
+    "overview": "Cinco jóvenes mutantes que acaban de descubrir sus habilidades, son encerrados en unas instalaciones secretas contra su voluntad y luchan por escapar de su pasado y salvarse a sí mismos",
+    "original_language": "en",
+    "original_title": "The New Mutants",
+    "poster_path": "/pBWrc52L3NmJLMZwfLzmP61ZO3.jpg",
+    "title": "Los nuevos mutantes"
+  },
+  {
+    "video": false,
+    "vote_average": 5.2,
+    "popularity": 662.127,
+    "vote_count": 13,
+    "release_date": "2020-10-14",
+    "adult": false,
+    "backdrop_path": "/h5sUE9jqoYrjsFjANJXL0gpZGye.jpg",
+    "id": 624779,
+    "genre_ids": [
+      28,
+      53,
+      27
+    ],
+    "overview": "Una noche típica para veteranos en un VFW se convierte en una batalla total por la supervivencia cuando un adolescente desesperado corre al bar con una bolsa de drogas robadas. Cuando una banda de punks violentos viene a buscarla, los veteranos usan todas las armas a su disposición para proteger a la niña y a ellos mismos de un ataque implacable.",
+    "original_language": "en",
+    "original_title": "VFW",
+    "poster_path": "/AnVD7Gn14uOTQhcc5xYZGQ9DRvS.jpg",
+    "title": "VFW"
+  },
+  {
+    "video": false,
+    "vote_average": 7.5,
+    "popularity": 660.767,
+    "vote_count": 2683,
+    "release_date": "2020-09-23",
+    "adult": false,
+    "backdrop_path": "/kMe4TKMDNXTKptQPAdOF0oZHq3V.jpg",
+    "id": 497582,
+    "genre_ids": [
+      80,
+      18,
+      9648
+    ],
+    "overview": "Enola Holmes, una intrépida joven que busca a su madre, utiliza su brillante instinto investigador para superar a su hermano Sherlock y ayudar a un lord en su fuga.",
+    "original_language": "en",
+    "original_title": "Enola Holmes",
+    "poster_path": "/8STWO9pUTqubPV04iXZ2R6SClME.jpg",
+    "title": "Enola Holmes"
+  },
+  {
+    "video": false,
+    "vote_average": 6.5,
+    "popularity": 644.215,
+    "vote_count": 57,
+    "release_date": "2020-02-27",
+    "adult": false,
+    "backdrop_path": "/4br4B8C0SRIYcKHUgoaOlGo50MU.jpg",
+    "id": 575088,
+    "genre_ids": [
+      27
+    ],
+    "overview": "La joven familia que se mudó a un nuevo apartamento en las afueras de la ciudad. La niñera contratada por ellos para la hija recién nacida rápidamente ganó confianza. Sin embargo, el niño mayor, Egor, habla del comportamiento aterrador de una mujer, pero sus padres no le creen. Las cámaras de vigilancia instaladas por el padre para mayor comodidad solo confirman que todo está en orden. Entonces, un día, Egor, al regresar a casa, no encuentra rastro ni de la niñera ni de la hermana pequeña, y los padres están en un trance extraño y ni siquiera recuerdan que tuvieron una hija. Entonces Egor, junto con sus amigos, va en busca, durante la cual resulta que la niñera es un antiguo demonio eslavo, conocido popularmente como Baba Yaga.",
+    "original_language": "ru",
+    "original_title": "Яга. Кошмар тёмного леса",
+    "poster_path": "/2EjUq3eqvDKU4EXh482IQcttcx4.jpg",
+    "title": "Baba Yaga: El regreso del demonio"
+  },
+  {
+    "video": false,
+    "vote_average": 7.8,
+    "popularity": 626.716,
+    "vote_count": 9,
+    "release_date": "2020-11-13",
+    "adult": false,
+    "backdrop_path": "/fTDzKoQIh1HeyjfpG5AHMi2jxAJ.jpg",
+    "id": 682377,
+    "genre_ids": [
+      28,
+      35
+    ],
+    "overview": "En ‘Chick Fight’, Malin interpreta a Anna Wyncomb, una mujer triste por el reciente fallecimiento de su madre. Y que pronto descubre que su padre es en realidad un hombre gay. Además de que poco después sufre la pérdida de su cafetería en un incendio, lo que la deja desolada y la lleva al borde de la desesperanza. Entonces, una amiga la invita a un club de pelea clandestino exclusivo para mujeres, sitio en el que conocerá a Olivia, el personaje interpretado por Bella Thorne, una peleadora intimidante y brutal con la que sostendrá una rivalidad. Mientras el guapo Alec Baldwin será el peleador retirado Jack Murphy que apoyará a Anna en su entrenamiento para la gran pelea.",
+    "original_language": "en",
+    "original_title": "Chick Fight",
+    "poster_path": "/4ZocdxnOO6q2UbdKye2wgofLFhB.jpg",
+    "title": "Chick Fight"
+  },
+  {
+    "title": "Ava",
+    "vote_average": 5.6,
+    "popularity": 602.583,
+    "overview": "Ava es una mortífera asesina a sueldo que trabaja para una organización de operaciones encubiertas, que viaja por todo el mundo acabando con aquellos objetivos que nadie más puede derribar. Cuando uno de sus encargos sale mal, Ava tendrá que luchar por una vida.",
+    "release_date": "2020-07-02",
+    "adult": false,
+    "backdrop_path": "/54yOImQgj8i85u9hxxnaIQBRUuo.jpg",
+    "id": 539885,
+    "genre_ids": [
+      28,
+      80,
+      18,
+      53
+    ],
+    "video": false,
+    "original_language": "en",
+    "original_title": "Ava",
+    "poster_path": "/qzA87Wf4jo1h8JMk9GilyIYvwsA.jpg",
+    "vote_count": 653
+  },
+  {
+    "video": false,
+    "vote_average": 7.6,
+    "popularity": 595.648,
+    "vote_count": 76,
+    "release_date": "2020-10-13",
+    "adult": false,
+    "backdrop_path": "/kU7ZiyeUwcpTkYj3UcxSqGdZmxY.jpg",
+    "id": 618353,
+    "genre_ids": [
+      16,
+      28
+    ],
+    "overview": "Producida, dirigida y escrita por Brandon Vietti, \"Death in the Family\" ofrece una nueva perspectiva sobre una historia de sobra conocida por los aficionados del Caballero Oscuro. En ella, el asesinato de Jason Todd provocará que el futuro de Batman, Robin y Joker tomen nuevos derroteros en los que el espectador puede incidir con sus propias decisiones.  Primera propuesta de película interactiva del universo DC.",
+    "original_language": "en",
+    "original_title": "Batman: Death in the Family",
+    "poster_path": "/gHNPuaZLXtiIowtIjzYDdYnITJU.jpg",
+    "title": "Batman: Death in the Family"
+  }
+]
+
+let generos = [
+  {
+    "id": 28,
+    "name": "Action"
+  },
+  {
+    "id": 12,
+    "name": "Adventure"
+  },
+  {
+    "id": 16,
+    "name": "Animation"
+  },
+  {
+    "id": 35,
+    "name": "Comedy"
+  },
+  {
+    "id": 80,
+    "name": "Crime"
+  },
+  {
+    "id": 99,
+    "name": "Documentary"
+  },
+  {
+    "id": 18,
+    "name": "Drama"
+  },
+  {
+    "id": 10751,
+    "name": "Family"
+  },
+  {
+    "id": 14,
+    "name": "Fantasy"
+  },
+  {
+    "id": 36,
+    "name": "History"
+  },
+  {
+    "id": 27,
+    "name": "Horror"
+  },
+  {
+    "id": 10402,
+    "name": "Music"
+  },
+  {
+    "id": 9648,
+    "name": "Mystery"
+  },
+  {
+    "id": 10749,
+    "name": "Romance"
+  },
+  {
+    "id": 878,
+    "name": "Science Fiction"
+  },
+  {
+    "id": 10770,
+    "name": "TV Movie"
+  },
+  {
+    "id": 53,
+    "name": "Thriller"
+  },
+  {
+    "id": 10752,
+    "name": "War"
+  },
+  {
+    "id": 37,
+    "name": "Western"
+  }
+]
+
+Enviar mensaje a #👨🏻💻👩🏻💻frontend
---
+++ semana6/Dia1/ejercicio2.js
@@ -0,0 +1,8 @@
+// ejercicio 2
+// iterando/recorriendo el arreglo de peliculas obtener cada pelicula y crear una tabla con las sgtes columnas ID, nombre, año e idioma
+
+// extra: añadir ora columna antes que las mencionadas que muestren el nro orden (1,2,3,4,5,...)
+
+// 1. obtengan el elemento
+// 2. cree los o el elemento necesario para una tabla columnas y demas
+// 3. muestren el contenido en el html
