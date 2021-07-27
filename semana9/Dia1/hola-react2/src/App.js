3a715282eb47becc3054d8d17a9c269e04156802
rmerino0401 <rmerino0401@gmail.com>
2021-07-27 18:14:30 -0500

    26.02.21 node y react 2da clase
    repaso con el profe osmar

---
+++ semana9/Dia1/hola-react2/src/App.js
@@ -0,0 +1,18 @@
+
+
+export default function App() {
+  let titulo = "Mi App2"
+
+  return (
+    <div>
+      <h1>{titulo}</h1>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, obcaecati odit voluptate consectetur dignissimos ea praesentium qui illo impedit harum, id nobis placeat nisi neque soluta, alias omnis sed aliquid?
+      </p>
+      <hr/>
+      <br/>
+    </div>
+    )
+    // jsx siempre sera entre parentesis como lo que vemos arriba
+  // toda etiqueta que se use en jsx debe estar cerrada
+}
